e7d134beb971a5891ab6c494be17a74a237d6fbf
Félix <felix.potente@alten.es>
2021-06-18 09:13:35 +0200

    Tema 21 Fin video "13. Restringir el Zoom y uso del Range" , "14. Obtener las coordenadas centrales del mapa" Y "15. Marcadores en el mapa"

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef,  ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
-  styles: [
+  styles: [`
+  .mapa-container{
+    width:100%;
+    height:100%
+  }
+
+  `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements  AfterViewInit{
+
+  @ViewChild('mapa') divMapa!:ElementRef;
+
+
+  mapa!:mapboxgl.Map;
+  zoomLevel:number=10;
+  center:[number,number] =[ -4.584619770182373, 41.58882980667756];
+
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngAfterViewInit(): void {
+
+    this.mapa = new mapboxgl.Map({
+       container: this.divMapa.nativeElement,
+       style: 'mapbox://styles/mapbox/streets-v11',
+       center:this.center,
+       zoom:this.zoomLevel
+       });
+        
+       const markerHtml:HTMLElement=document.createElement('div');
+       markerHtml.innerHTML='Hola Mundo';
+
+       const maker = new mapboxgl.Marker(
+        markerHtml
+       )
+       .setLngLat(this.center)
+       .addTo(this.mapa);
+
+
+      }
+
+
 
 }
